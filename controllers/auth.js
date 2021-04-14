13b9b9782a1b0d30cd105ca718a5c903a01805c1
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-14 10:18:41 +0200

    initial commits

---
+++ controllers/auth.js
@@ -0,0 +1,28 @@
+const { response } = require('express');
+
+const createUser = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'register',
+  });
+};
+
+const userLogin = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'login',
+  });
+};
+
+const revalidateToken = (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'renew',
+  });
+};
+
+module.exports = {
+  createUser,
+  userLogin,
+  revalidateToken,
+};
---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+/*
+  Rutas de Usuarios / Auth
+  host + /api/auth
+*/
+
+const { Router } = require('express');
+const router = Router();
+
+const { createUser, userLogin, revalidateToken } = require('../controllers/auth');
+
+router.post('/new', createUser);
+
+router.post('/', userLogin);
+
+router.get('/renew', revalidateToken);
+
+module.exports = router;
