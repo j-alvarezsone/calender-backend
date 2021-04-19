7f38ce3ee44d8efe1991695e6b68413c8b4315d6
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-19 09:18:22 +0200

    added routes, express.json

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 // Crear el servidor de express
 const app = express();
 
-// Directorio publico
+// Directorio publico, use = middleware
 app.use(express.static('public'));
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Rutas
+//* Todos los archivos que van a exportar desde './routes/auth', lo vamos a utilizar en el '/api/auth'
 // TODO: auth // crear, login, renew
 app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD: Eventos
