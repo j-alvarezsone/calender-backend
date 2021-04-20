1f34578aa65b097a4446430a49bf344633a3aaba
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-20 08:30:34 +0200

    removed validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,6 @@ const { validationResult } = require('express-validator');
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  // manejos de errores
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
-
   res.status(201).json({
     ok: true,
     msg: 'register',
@@ -24,14 +15,6 @@ const createUser = (req, res = response) => {
 
 const userLogin = (req, res = response) => {
   const { email, password } = req.body;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.json({
     ok: true,
