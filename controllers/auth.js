168ceca43d3d3725b4ee6742c7ca12808079e65b
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-20 10:26:16 +0200

    added user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,24 @@
 const { response } = require('express'); // recuperar el intellisense
-const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+  try {
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Please be in contact with the administrator',
+    });
+  }
 };
 
 const userLogin = (req, res = response) => {
