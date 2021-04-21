222a5553f96fa7829924ac81424c4a38d7eb15f3
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-21 09:07:50 +0200

    added generateJWT function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express'); // recuperar el intellisense
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 const createUser = async (req, res = response) => {
   const { email, password } = req.body;
 
@@ -23,11 +24,13 @@ const createUser = async (req, res = response) => {
     await user.save();
 
     // Generar JWT
+    const token = await generateJWT(user.id, user.name);
 
     res.status(201).json({
       ok: true,
       id: user.id,
       name: user.name,
+      token,
     });
   } catch (err) {
     res.status(500).json({
@@ -60,11 +63,14 @@ const userLogin = async (req, res = response) => {
       });
     }
 
-    // Generar nuestro JWT
+    // Generar JWT
+    const token = await generateJWT(user.id, user.name);
+
     res.json({
       ok: true,
       uid: user.id,
       name: user.name,
+      token,
     });
   } catch (err) {
     res.status(500).json({
