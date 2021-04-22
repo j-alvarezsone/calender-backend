3161ae19ffd7dedacd52dd9e7d4a0272e1ae434e
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-04-22 11:46:05 +0200

    added CRUD

--- controllers/events.js
+++ controllers/events.js
@@ -1,31 +1,109 @@
 const { response } = require('express'); // recuperar el intellisense
+const Event = require('../models/Event');
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+  // Retornar todos los eventos
+  const events = await Event.find().populate('user', 'name');
   res.json({
     ok: true,
-    msg: 'Get events',
+    events,
   });
 };
 
-const createEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Create event',
-  });
+const createEvent = async (req, res = response) => {
+  const event = new Event(req.body);
+
+  try {
+    event.user = req.uid;
+
+    const eventDb = await event.save();
+
+    res.json({
+      ok: true,
+      event: eventDb,
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Please be in contact with the administrator',
+    });
+  }
 };
 
-const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Update event',
-  });
+const updateEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Event does not exist with that id',
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have the privilege to edit this event',
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid,
+    };
+
+    const updateEvent = await Event.findByIdAndUpdate(eventId, newEvent, { new: true });
+
+    res.json({
+      ok: true,
+      event: updateEvent,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please be in contact with the administrator',
+    });
+  }
 };
 
-const deleteEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Delete event',
-  });
+const deleteEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Event does not exist with that id',
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'You do not have the privilege to delete this event',
+      });
+    }
+
+    await Event.findByIdAndDelete(eventId);
+
+    res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please be in contact with the administrator',
+    });
+  }
 };
 
 module.exports = {
